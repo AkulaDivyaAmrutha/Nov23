2885edba3e48cf5532a469c8bdf01d15e3aae50e
Akula Divya Amrutha <125580762+AkulaDivyaAmrutha@users.noreply.github.com>
2023-11-24 06:22:41 +0530

    Create ass2.py

---
+++ ass2.py
@@ -0,0 +1,11 @@
+n=int(input())
+l=list(map(int,input().split()))
+s,e=map(int,input().split())
+bs=l[0:s]
+afs=l[e+1:n]
+sr=l[s:e+1]
+sr.sort()
+sr.reverse()
+res=bs+sr+afs
+for i in res:
+    print(i,end=' ')
